e5c853e298ad639fe15429ff01e33ca80ab0cb01
t3kt <tommy.etkin@gmail.com>
2015-05-04 21:29:08 -0400

    fixes to the flowpt selector components

--- scripts/build_module_flowpoint_list.py
+++ scripts/build_module_flowpoint_list.py
@@ -2,14 +2,15 @@ def cook(dat):
 	dat.clear()
 	mods = dat.inputs[0]
 	dat.appendRow(['name', 'label', 'path', 'type'])
-	for m in mods.col('name')[1:]:
-		mname = mods[m, 'name']
+	for mname in mods.col('name')[1:]:
+		if mods[mname, 'fake'] == '1':
+			continue
 		dat.appendRow([mname + ':dry'])
 		dat[mname+':dry', 'label'] = mname + ' in'
-		dat[mname+':dry', 'path'] = mods[m, 'dry']
+		dat[mname+':dry', 'path'] = mods[mname, 'dry']
 		dat[mname+':dry', 'type'] = 'nodein'
 		dat.appendRow([mname + ':wet'])
 		dat[mname+':wet', 'label'] = mname + ' out'
-		dat[mname+':wet', 'path'] = mods[m, 'wet']
+		dat[mname+':wet', 'path'] = mods[mname, 'wet']
 		dat[mname+':wet', 'type'] = 'nodeout'
 	return
