60592f52e638a09fa908e33277e028f8dceec5dc
t3kt <tommy.etkin@gmail.com>
2015-05-15 21:40:26 -0400

    fixed a bug in the module definition processor that was adding an extra property to the table

--- scripts/pre_process_modules.py
+++ scripts/pre_process_modules.py
@@ -16,7 +16,7 @@ def getDefault(defaults, propname, modtype):
 def cook(dat):
 	dat.copy(dat.inputs[0])
 	defs = dat.inputs[1]
-	for c in defs.col(0):
+	for c in defs.col(0)[1:]:
 		if dat[0, c] is None:
 			dat.appendCol(c)
 	cols = [c.val for c in dat.row(0)[1:]]
